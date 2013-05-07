a38f0806d3ff703568a048b2a7d237e82b4fb2d2
Massimo Gervasini <mgervasini@mozilla.com>
2013-05-07 18:29:32 +0200

    added tox to setup.py, updated tox enviroments to run tests agaist different versions of hg and python

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 #! /usr/bin/env python
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+import sys
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
 
 setup(
     name="buildtools",
@@ -32,4 +47,8 @@ setup(
 
     # include files listed in MANIFEST.in
     include_package_data=True,
+
+    #tox
+    tests_require=['tox'],
+    cmdclass = {'test': Tox},
 )
